a220cf656e096963ad6c8d272a9cb8d499e2ee15
miokato <miokato@me.com>
2019-10-05 18:37:35 +0900

    first commit.

--- ARWithOSCStudy/ViewController.swift
+++ ARWithOSCStudy/ViewController.swift
@@ -9,67 +9,61 @@
 import UIKit
 import SceneKit
 import ARKit
+import F53OSC
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate  {
 
     @IBOutlet var sceneView: ARSCNView!
+
+    var shipNode: SCNNode?
     
+    let server = F53OSCServer.init()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
+
         sceneView.delegate = self
+        sceneView.session.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.scene = SCNScene(named: "art.scnassets/ship.scn")!
+        shipNode = sceneView.scene.rootNode.childNodes.first!
+        shipNode?.simdPosition = simd_float3(0, -0.1, -0.8)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        // osc
+        server.port = 8080
+        server.delegate = self
+        
+        if server.startListening() {
+            print("server listning \(server.port)")
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
+}
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+extension ViewController: F53OSCPacketDestination {
+    func take(_ message: F53OSCMessage?) {
+        guard let node = shipNode else {return}
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        let value: Int = message?.arguments[0] as! Int
+        let smoothed = round(Float(value) * 5.0) / 5.0
+        let deg = round(smoothed / 1024.0 * 360)
+        let rad = 2 * Float.pi * deg / 360
         
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        node.eulerAngles.y = rad
     }
 }
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ARWithOSCStudy' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ARWithOSCStudy
+  pod 'F53OSC', :git => 'https://github.com/Figure53/F53OSC.git'
+
+  target 'ARWithOSCStudyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
